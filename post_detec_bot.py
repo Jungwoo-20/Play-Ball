66fdf8b76aaf5a5e21468bef54ce86f01a9e2f33
Jungwoo-20 <62051794+Jungwoo-20@users.noreply.github.com>
2020-12-20 15:51:56 +0900

    Post detection

--- post_detec_bot.py
+++ post_detec_bot.py
@@ -2,20 +2,28 @@ import threading
 import pymysql
 import run_bert_answer
 
+question_list = []
+
 
 def detec_bot():
     try:
-        conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='1234', db='playball', charset='utf8')
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
-        sql = 'SELECT * FROM playball.board where answer is null order by date asc limit 1;'
-        cursor.execute(sql)
-        rows = cursor.fetchall()
-        for row in rows:
-            question = row['question']
-            no = row['No']
+        question_param = question_list.pop(0).split('!')
+        no = question_param[0]
+        question = question_param[1]
         run_bert_answer.run_bert_answer(no, question)
     except:
-        pass
+        try:
+            conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='1234', db='playball',
+                                   charset='utf8')
+            cursor = conn.cursor(pymysql.cursors.DictCursor)
+            sql = 'SELECT * FROM playball.board where answer is null order by date asc limit 1;'
+            cursor.execute(sql)
+            rows = cursor.fetchall()
+            for row in rows:
+                question = row['question']
+                no = row['No']
+            run_bert_answer.run_bert_answer(no, question)
+        except:
+            cursor.close()
     finally:
-        cursor.close()
-        threading.Timer(7, detec_bot).start()
+        threading.Timer(3, detec_bot).start()
